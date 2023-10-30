cfce23595049bf6f05745af782b1cfdd4af597cf
LasFar <104170187+OhLasFar@users.noreply.github.com>
2023-10-30 13:01:01 -0400

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "LasFar",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Make it happen",
     bio: "I am an analyst and a fast learner.",
   },
